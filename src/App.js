f6a44001657573e6a4c0e76aa03f81ebeba4a3f0
wangli147 <1475986800@qq.com>
2017-07-24 20:47:42 +0800

    首页基本写完

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Carousel } from 'antd';
 import './App.css';
+import $ from 'jquery';
 import Home from './js/Home';
 import Finance from './js/Finance';
 import logo from './imgs/logo.png';
+import logos from './imgs/logo2.png';
 import {
   BrowserRouter as Router,
   Route,
@@ -11,14 +13,33 @@ import {
 } from 'react-router-dom'
 
 class App extends Component {
+  componentDidMount(){
+  	$(function(){
+  		$('.wl_nav li').click(function(){
+  			$(this).addClass('active').siblings().removeClass('active');
+  		})
+  	})
+  }
   render() {
     return (
       <Router>
       <div>
   	    <div className="wl_fixed">
-            <div className="wl_header clear">
+            <div className="wl_header clear" id="header">
                 <div className="wl_merge clear">
                     <Link to="/"  className="wl_logo left"><img src={logo} alt="logo"/></Link> 
+                     <ul className="right wl_nav">
+                        <li className="active"><Link to="/">首页</Link></li>
+                        {/*<li><Link to="/Qiye">企业金融</Link></li>*/}
+                        <li><Link to="/finance">个人金融</Link></li>
+                         {/*<li><Link to="/About">关于我们</Link></li>
+                        <li><Link to="/News">公告新闻</Link></li>*/}
+                     </ul>          
+                </div>  
+            </div>
+             <div className="wl_header clear" id="headerS">
+                <div className="wl_merge clear">
+                    <Link to="/"  className="wl_logo left"><img src={logos} alt="logos"/></Link> 
                      <ul className="right wl_nav">
                         <li className="active"><Link to="/">首页</Link></li>
                         {/*<li><Link to="/Qiye">企业金融</Link></li>*/}
--- src/js/Home.js
+++ src/js/Home.js
@@ -2,18 +2,55 @@ import React, { Component } from 'react';
 import { Carousel } from 'antd';
 import '.././App.css';
 import '../css/home.css';
+import $ from 'jquery';
 import phone1 from '../imgs/phone1.png';
 import phone2 from '../imgs/phone2.png';
 import phone3 from '../imgs/phone3.png';
 import penguin from '../imgs/qier.png';
+import pic1 from '../imgs/pic1.jpg';
+import pic2 from '../imgs/pic2.jpg';
+import weixin from '../imgs/1.png';
+import er1 from '../imgs/w1.png';
+import er2 from '../imgs/qrcode.png';
 
 
 import Footer from './Footer'
 class Home extends Component {
+	componentDidMount(){
+		var deviceLeft=document.getElementById('deviceLeft');
+		var scroll=document.getElementById('scroll');
+		var header=document.getElementById('header');			
+		var headerS=document.getElementById('headerS');
+		var headerS=document.getElementById('headerS');
+		var iphone=document.getElementById('iphone');
+		var t = scroll.offsetTop;
+		window.addEventListener('scroll',function(){
+			 var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
+		        console.log(scrollTop,t);
+		        if(scrollTop>t){
+		            headerS.style.display='block';
+		            headerS.style.transition='.5s';
+		            headerS.style.height='60px';
+		            header.style.display='none';
+		            deviceLeft.style.position = 'fixed';		            
+		        }
+		        else{
+		            header.style.position = 'fixed';
+		            deviceLeft.style.position = 'absolute';
+		            header.style.top = '0';
+		             deviceLeft.style.top = '50px';
+		            header.style.display='block';
+		            headerS.style.display='none';
+		        }
+			console.log(scrollTop)
+		})
+		
+		
+	}
   render() {
     return (
 	    <div>
-	    	<div className="wl_home modPage modBand">
+	    	<div className="wl_home modPage modBand" id="no">
 	    		{/*首页bg*/}
 	    		<div className="wl_boxed">
 	    			<div className="wl_vertical">
@@ -39,8 +76,69 @@ class Home extends Component {
 	    					</div>
 	    				</div>
 	    			</div>
+	    			<div className="wl_button">
+	    				<i className="wl_icon"></i>
+	    			</div>
 	    		</div>
 	    	</div>
+	    	<div className="modPage wl_scroll modBand" id="scroll">
+    			<div className="wl_wraper" id="deviceLeft">
+    				<div className="wl_vertical">
+    					<div className="wl_align">
+    						<div className="wl_device">
+    							<div className="wl_container">
+    								<Carousel autoplay>
+									    <div><img src={pic1} /></div>
+									    <div><img src={pic2} /></div>
+									 </Carousel>
+    							</div>
+    						</div>
+    					</div>
+    				</div>
+				</div>
+				<div className="text-wraper">
+					<div className="step_text">
+						<div className="wl_vertical">
+							<div className="wl_align">
+								<img src={weixin} className="right_img"/>
+								<h2 className="subtitle">微粒贷</h2>
+								<h3 className="subtitleBom">让您的微小心愿立刻实现！</h3>
+								<div className="wl_weixin">
+									<div className="wl_leftWei">
+										<img src={er1}/>
+										请用微信扫描二维码
+									</div>
+									<div className="wl_RightQQ">
+										<img src={er2}/>
+										请用手机QQ扫描二维码
+									</div>
+								</div>
+								<p className="wl_details">
+									<a href="#">了解详情</a>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div className="step_text">
+						<div className="wl_vertical">
+							<div className="wl_align">
+								<img src={weixin} className="right_img"/>
+								<h2 className="subtitle">微众银行APP</h2>
+								<h3 className="subtitleBom">We给您带来更舒适便捷的银行服务体验！</h3>
+								<div className="wl_weixin">
+									<div className="wl_leftWei">
+										<img src={er1}/>
+										请用微信扫描二维码
+									</div>
+								</div>
+								<p className="wl_details detOrange">
+									<a href="#">了解详情</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+    		</div>
 	     	{/*<Footer />*/}
 	    </div>
 
