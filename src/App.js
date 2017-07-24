8fb53b3cd01ffd307abf7daa4246481bef51dd50
wangli147 <1475986800@qq.com>
2017-07-24 11:51:12 +0800

    home开头

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Carousel } from 'antd';
 import './App.css';
-import Home from './js/Home'
+import Home from './js/Home';
+import logo from './imgs/logo.png';
 import {
   BrowserRouter as Router,
   Route,
@@ -12,15 +13,28 @@ class App extends Component {
   render() {
     return (
       <Router>
-	    <div>
-	      <ul>
-	        <li><Link to="/">Home</Link></li>
-	      </ul>
+      <div>
+  	    <div className="wl_fixed">
+            <div className="wl_header clear">
+                <div className="wl_merge clear">
+                    <Link to="/"  className="wl_logo left"><img src={logo} alt="logo"/></Link> 
+                     <ul className="right wl_nav">
+                        <li className="active"><Link to="/">首页</Link></li>
+                        {/*<li><Link to="/Qiye">企业金融</Link></li>
+                        <li><Link to="/Self">个人金融</Link></li>
+                        <li><Link to="/About">关于我们</Link></li>
+                        <li><Link to="/News">公告新闻</Link></li>*/}
+                     </ul>          
+                </div>  
+            </div>
+        </div>
 
-	      <hr/>
-
-	      <Route exact path="/" component={Home}/>
-	    </div>
+  	      <Route exact path="/" component={Home}/>
+          {/*<Route exact path="/Qiye" component={Qiye}/>
+          <Route exact path="/Self" component={Self}/>
+          <Route exact path="/About" component={About}/>
+          <Route exact path="/News" component={News}/>*/}
+      </div>
   </Router>
     );
   }
--- src/js/Home.js
+++ src/js/Home.js
@@ -1,11 +1,47 @@
 import React, { Component } from 'react';
 import { Carousel } from 'antd';
 import '.././App.css';
+import '../css/home.css';
+import phone1 from '../imgs/phone1.png';
+import phone2 from '../imgs/phone2.png';
+import phone3 from '../imgs/phone3.png';
+import penguin from '../imgs/qier.png';
+
+
+import Footer from './Footer'
 class Home extends Component {
   render() {
     return (
 	    <div>
-	     开始就会疯狂
+	    	<div className="wl_home modPage modBand">
+	    		{/*首页bg*/}
+	    		<div className="wl_boxed">
+	    			<div className="wl_vertical">
+	    				<div className="wl_align clear">
+	    					<div className="wl_left left">
+	    						<h1 className="wl_title delay-1"> 
+	    							腾讯牵头发起设立的银行
+	    						</h1>
+	    						<p className="wl_pull">
+	    							从一次惊喜
+	    							<span>到每次相伴</span>
+	    						</p>
+	    					</div>
+	    					<div className="wl_right right">
+	    						<div className="wl_align">
+    								<div className="wl_images">
+	    								<img className="wl_phoneO" src={phone1}/>
+	    								<img className="wl_phoneS" src={phone2}/>
+	    								<img className="wl_phoneT" src={phone3}/>
+	    								<img className="wl_penguin a-delay" src={penguin}/>
+	    							</div>
+	    						</div>
+	    					</div>
+	    				</div>
+	    			</div>
+	    		</div>
+	    	</div>
+	     	{/*<Footer />*/}
 	    </div>
 
     );
